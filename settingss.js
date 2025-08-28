7f70cf5cdb0bfaee5131085b9225848005a10ed8
Heatless <dianwilson66@gmail.com>
2025-08-28 01:31:58 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~M21S0JaK#_Vyib_wcRg3oOr5B-2gie2oYD2pn1G8D80dA2NROHeA",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
